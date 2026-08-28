65a59c9eab2f6fc8e01bf085a9737673abb3d62f
bachan37 <bachan37@gmail.com>
2026-08-28 19:24:51 +0530

    endpoint for read a resource

--- crud.py
+++ crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 import models, schemas
+import ipdb
 
 def get_task_by_title(db: Session, title: str):
   return db.query(models.Task).filter(models.Task.title == title).first()
@@ -7,12 +8,11 @@ def get_task_by_title(db: Session, title: str):
 def get_tasks(db: Session, skip: int = 0, limit: int = 100):
   return db.query(models.Task).offset(skip).limit(limit).all()
 
+def get_task(db: Session, task_id: int):
+  return db.query(models.Task).filter(models.Task.id == task_id).first()
+
 def create_task(db: Session, task: schemas.TaskCreate):
-  db_task = models.Task(
-    title=task.title,
-    description=task.description,
-    completed=task.completed
-  )
+  db_task = models.Task(**task.model_dump())
   
   db.add(db_task)
   db.commit()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, status, HTTPException
+import ipdb
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -11,9 +12,9 @@ app = FastAPI(title="Task Manager API")
 
 @app.post("/tasks/", response_model=schemas.TaskResponse, status_code=status.HTTP_201_CREATED)
 async def create_new_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
-  task = crud.get_task_by_title(db=db, title=task.title)
+  db_task = crud.get_task_by_title(db=db, title=task.title)
 
-  if task:
+  if db_task is not None:
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
         detail="A task with this title already exists."
@@ -21,6 +22,20 @@ async def create_new_task(task: schemas.TaskCreate, db: Session = Depends(get_db
 
   return crud.create_task(db=db, task=task)
 
-@app.get("/tasks/", response_model=List[schemas.TaskResponse])
+@app.get("/tasks/", response_model=List[schemas.TaskResponse], status_code=status.HTTP_200_OK)
 async def read_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
   return crud.get_tasks(db=db, skip=skip, limit=limit)
+
+
+@app.get("/tasks/{task_id}", response_model=schemas.TaskResponse, status_code=status.HTTP_200_OK)
+async def read_task(task_id: int, db: Session = Depends(get_db)):
+  db_task = crud.get_task(db=db, task_id=task_id)
+
+  if db_task is None:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND, 
+      detail="Task not found"
+    )
+
+  return db_task
+
